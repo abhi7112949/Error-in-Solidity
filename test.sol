d54b5621c3bf175530cc62e9e8499d20a23f9576
Abhijeet Sinha <abhijeet.sinha383@gmail.com>
2021-08-12 12:04:57 +0530

    added getName and setname

--- test.sol
+++ test.sol
@@ -1,10 +1,19 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract MyProject {
     
-    bytes32 _name = 'Abhijeet';
+    bytes32 name = 'Abhijeet';
     
-    function name() public view returns (bytes32) {
-        return _name;        
+    function getName() public view returns (bytes32) {
+        return name;        
+    }
+
+    function add(uint256 a, uint256 b) external pure returns(uint256) {
+        return a + b;
+    }
+
+    function setName(bytes32 _name) public {
+        name = _name;
     }
 }
